aa9b1aa155ffe89f79ede5ca9c456be1180dbaab
Toni <windtronic89@gmail.com>
2023-02-27 08:35:59 -0700

    working

--- go_find_anime/src/components/AnimeCard.jsx
+++ go_find_anime/src/components/AnimeCard.jsx
@@ -3,10 +3,9 @@ import React from "react"
 export default function AnimeCard (props) {
     return (
         <div>
-            <div className='anime-card'>
-                
-            </div>
-
+            {
+           
+}
         </div>
     )
 }
--- go_find_anime/src/components/AnimeData.js
+++ go_find_anime/src/components/AnimeData.js
@@ -1,63 +1,56 @@
 import { useState, useEffect } from "react"
-import { Link, useNavigate, useParams } from 'react-router-dom'
 import axios from 'axios'
-import React from "react"
 
 
 
 
+export default function AnimeData () {
+    const [anime, setAnime] = useState({})	
+   
+
+useEffect(() => {
+	const url = 'https://api.jikan.moe/v4/anime'
+   
+    
+    const getAnime = async() => {
+		const response = await axios.get(url)
+        console.log(response.data)
+		console.log(response.data.data[0])
+        // console.log(response.list)
+        
+       
+        setAnime(response.data)
+		
+}
+	getAnime()
+   
+}, [])	
+console.log(anime)
 
 
-export default function AnimeData (props) {
- let navigate = useNavigate()
- const showAnime = (index) => {
-    navigate(`${index}`)
- }
+// let navigate = useNavigate()
+// const showAnimeList = (index) => {
+//     navigate(`${index}`)
+// }
 
 
-    //making a state to set data in
+
+
+return (
+    <div>
+    <h2></h2>
+    </div>
+    
+
+       
+       
+    )
+    
+}
+
+//making a state to set data in
     // setting up a useEffect to control my components lifecycle
     //organize API links /url
     //Make the API call
     // set our data in state and log it
     // render our data
-
- 	const [animeList, setAnimeList] = useState([])	
-	// const [search, setSearch] = useState([])
-
-useEffect(() => {
-	const url = 'https://api.jikan.moe/v4/anime'
-    
-    
-    const getAnimeList = async() => {
-		const response = await axios.get(url)
-		console.log(response.data)
-        setAnimeList(response.data)
-		
-}
-	getAnimeList()
-
-}, [])	
-
-// useEffect(() => {
-//    const getSearch = async() => {
-//     const response = await axios.get(getSearch)
-//     console.log(response.data)
-//     setSearch(response.data)
-
-// }
-
-    // getSearch()
-// }, [])
-
-    return (
-        <div>
-        <div className='anime-grid' id='anime'>
-          
-            
-            </div>
-       
-        </div>
-    )
-    
-}
